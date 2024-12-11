56c432eccd7db48a43b1f26fb6fdb0e36bf0ac06
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-12-11 16:01:52 -0500

    Debug button collapsing on iOS

--- frontend-royale/components/DefaultButton.tsx
+++ frontend-royale/components/DefaultButton.tsx
@@ -2,15 +2,15 @@ import { LinearGradient } from "expo-linear-gradient";
 import { Href, Link } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
-import { scale, moderateScale, verticalScale } from 'react-native-size-matters';
+import { scale, moderateScale, verticalScale } from "react-native-size-matters";
 import CustomText from "./CustomText";
 import { buttonGradient } from "@/utils/commonColors";
 
 type DefaultButtonType = {
-    href?: Href<string | object>,
-    name?: string,
-    onPress?: () => void
-}
+  href?: Href<string | object>;
+  name?: string;
+  onPress?: () => void;
+};
 
 const DefaultButton: React.FC<DefaultButtonType> = ({
   href,
@@ -23,22 +23,22 @@ const DefaultButton: React.FC<DefaultButtonType> = ({
         <Link
           href={href}
           style={{
-            width: '100%',
-            height: '100%',
-            textAlign: 'center',
-            textAlignVertical: 'center',
+            width: "100%",
+            height: "100%",
+            textAlign: "center",
+            textAlignVertical: "center",
           }}
         >
           <LinearGradient
             colors={buttonGradient}
             style={{
               ...styles.actionButtonView,
-              minWidth: href ? moderateScale(180) : 'auto',
+              minWidth: href ? moderateScale(180) : "auto",
             }}
             start={[0, 1]}
             end={[1, 0]}
           >
-            <CustomText style={styles.actionButtonText} weight='Bold'>
+            <CustomText style={styles.actionButtonText} weight="Bold">
               {name}
             </CustomText>
           </LinearGradient>
@@ -48,45 +48,48 @@ const DefaultButton: React.FC<DefaultButtonType> = ({
           colors={buttonGradient}
           style={{
             ...styles.actionButtonView,
-            minWidth: href ? moderateScale(180) : 'auto',
+            minWidth: href ? moderateScale(180) : "auto",
           }}
           start={[0, 1]}
           end={[1, 0]}
         >
-          <CustomText style={styles.actionButtonText} weight='Bold'>
+          <CustomText style={styles.actionButtonText} weight="Bold">
             {name}
           </CustomText>
         </LinearGradient>
       )}
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   actionButtonText: {
     fontSize: scale(30), // Scaled font size
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     paddingHorizontal: 20,
     // paddingVertical: 2,
     paddingTop: verticalScale(5),
     paddingBottom: verticalScale(5),
     // paddingLeft: moderateScale(20),
     // paddingRight: moderateScale(20),
+    lineHeight: 20,
+    // backgroundColor: "red",
   },
   actionButtonView: {
+    width: "100%",
+    paddingVertical: verticalScale(11),
     borderRadius: moderateScale(10), // Scaled border radius
     borderWidth: 2,
-    borderColor: 'white',
-    width: '100%',
+    borderColor: "white",
     // flex: 1, // why used this
   },
   actionButton: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     maxHeight: verticalScale(60), // Scaled max height
   },
-})
+});
 
 export default DefaultButton;
